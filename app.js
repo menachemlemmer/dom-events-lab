dc5736c9ee3fe75a60807724a1780d105e5d02f8
menachemlemmer <nmmlemmer@gmail.com>
2024-05-10 13:16:19 -0400

    Add multi-digit and operation chaining functionality

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
 /*-------------------------------- Constants --------------------------------*/
 
+/*-------------------------------- Variables --------------------------------*/
+
+let currentNumber = "";
+let previousNumber = "";
+let currentOperator = "";
+let calculated = false;
+
+/*------------------------ Cached Element References ------------------------*/
+
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equalBtn = document.querySelector(".equals");
-
-/*-------------------------------- Variables --------------------------------*/
-
-let currentNumber;
-let previousNumber;
-let currentOperator;
-
-/*------------------------ Cached Element References ------------------------*/
+const clearBtn = document.querySelector(".clear");
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    previousNumber = currentNumber;
-    currentNumber = Number(event.target.innerText);
+    currentNumber += event.target.innerText;
+    console.log(currentNumber);
   });
 });
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (event) => {
-    currentOperator = event.target.innerText;
+    if (previousNumber === "") {
+      currentOperator = event.target.innerText;
+      previousNumber = currentNumber;
+      currentNumber = "";
+    } else {
+      equal();
+      currentOperator = event.target.innerText;
+    }
   });
 });
 
-equalBtn.addEventListener("click", (event) => {
-  result = operate(previousNumber, currentNumber, currentOperator);
-  console.log(result);
-});
+equalBtn.addEventListener("click", equal);
+
+clearBtn.addEventListener("click", clear);
+
 /*-------------------------------- Functions --------------------------------*/
 
 function operate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
   if (operator === "+") {
     return num1 + num2;
   } else if (operator === "-") {
@@ -44,4 +55,17 @@ function operate(num1, num2, operator) {
     return num1 / num2;
   }
 }
-``;
+
+function equal() {
+  result = operate(previousNumber, currentNumber, currentOperator);
+  clear();
+  previousNumber = result;
+  console.log(result);
+}
+
+function clear() {
+  currentNumber = "";
+  previousNumber = "";
+  operator = "";
+  calculated = false;
+}
